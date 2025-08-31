5e7cd762935f653170b2f5a63e5a983349f761c0
Mohammed M. Hadi <mohammeditsec@gmail.com>
2025-08-31 11:04:10 +0300

    Add project files.

---
+++ ContosoPizza/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoPizza.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [Column(TypeName ="decimal(6,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
