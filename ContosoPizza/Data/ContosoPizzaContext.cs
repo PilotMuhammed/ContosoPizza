a376f81cf6213a4c284c99e6e24b0cfca82e0362
Mohammed M. Hadi <mohammeditsec@gmail.com>
2025-08-31 14:45:04 +0300

    Add Data File & Made Migrations

---
+++ ContosoPizza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,18 @@
+﻿using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Data
+{
+    public class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer>? Customers { get; set; }
+        public DbSet<Order>? Orders { get; set; }
+        public DbSet<OrderDetail>? OrderDetails { get; set; }
+        public DbSet<Product>? Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=ContosoPizzaDB;Integrated Security=True;Encrypt=False");
+        }
+    }
+}
--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,6 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using ContosoPizza.Data;
+using ContosoPizza.Models;
 
-app.MapGet("/", () => "Hello World!");
+using ContosoPizzaContext context = new ContosoPizzaContext();
 
-app.Run();
+var products = from product in context.Products
+               where product.Price > 10.00m
+               orderby product.Name
+               select product; 
+
+foreach (Product p in products)
+{
+    Console.WriteLine($"Id: {p.Id}");
+    Console.WriteLine($"Name: {p.Name}");
+    Console.WriteLine($"Price: {p.Price}");
+    Console.WriteLine(new string('-', 20));
+}
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.Run();
